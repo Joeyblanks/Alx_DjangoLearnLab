e1c3ce9ac768512e51d9f457b3ef04437ed11a66
Joeyblanks <blanksonrobin@gmail.com>
2025-07-19 15:09:26 +0000

    Initial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 
 # Create your views here.
