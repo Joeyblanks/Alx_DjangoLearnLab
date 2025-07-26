4ee5e43d5617d8390ea9637a64332f6a845fc8e6
Joeyblanks <blanksonrobin@gmail.com>
2025-07-26 19:43:29 +0000

    Initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required, login_required
 
 # Create your views here.
+@login_required
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    # view logic
+    pass
