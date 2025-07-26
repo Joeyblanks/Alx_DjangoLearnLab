7a6fdeef7dbd2064952aab5e90613b4ad8e170ee
Joeyblanks <blanksonrobin@gmail.com>
2025-07-26 20:05:45 +0000

    Initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
-class BookSearchForm(forms.Form):
-    query = forms.CharField(required=True, max_length=255)
-
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(widget=forms.Textarea, label='Your Message')
