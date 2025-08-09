06aaf1b0feee4aabe2c25b2d907d3e9a79926a40
Joeyblanks <blanksonrobin@gmail.com>
2025-08-09 20:10:32 +0000

    Initial commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 # Permissions: Read-only access for everyone, but create/update/delete is restricted to authenticated users
