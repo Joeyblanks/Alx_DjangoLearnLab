0fcd2a70cf812d4606c89264a9c89be4402169db
Joeyblanks <blanksonrobin@gmail.com>
2025-08-09 20:20:22 +0000

    Initial commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 
 # Create your views here.
 # Permissions: Read-only access for everyone, but create/update/delete is restricted to authenticated users
