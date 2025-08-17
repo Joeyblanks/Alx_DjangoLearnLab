115d8d46dc644a018234e62be1a656ca90768427
Joeyblanks <blanksonrobin@gmail.com>
2025-08-17 22:37:55 +0000

    Initial commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -163,7 +163,7 @@ def post_list(request):
     query = request.GET.get('q')
     if query:
         posts = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
+            Q(title__icontains=query) | Q(content__icontains=query) |  Q(tags__name__icontains=query)
         ).distinct()
     else:
         posts = Post.objects.all()
